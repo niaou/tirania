eab3baf83a4c046ed0aa3014c63506788ed1a67d
niaou <polemitist@gmail.com>
2025-08-27 19:42:34 +0300

    Initial commit

---
+++ src/main/java/com/niaou/tiraniabot/TiraniaApplication.java
@@ -0,0 +1,13 @@
+package com.niaou.tiraniabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiraniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiraniaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niaou/tiraniabot/TiraniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niaou.tiraniabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiraniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
